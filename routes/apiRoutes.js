ddb2e78ba7e298a1e393578d274011c9d007bc20
Ivy Bean <ivieking@gmail.com>
2020-10-29 16:20:48 -0400

    apiRoutes.js intial push

---
+++ routes/apiRoutes.js
@@ -0,0 +1,24 @@
+const db = require("../models/workout");
+const router = require("express").Router();
+
+//Display workouts
+router.get("/api/workouts", (req, res) => {
+
+});
+
+//Display workout range
+router.get("/api/workouts/range", (req, res) => {
+  
+});
+
+//Post new workout
+router.post("/api/workouts/", (req, res) => {
+  
+});
+
+
+router.put("/api/workouts/range", (req, res) => {
+  
+});
+
+module.exports = router;
